cb7f72539acc4d52c1506c5734afd70b24ca9be3
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2024-10-19 23:10:57 +0800

    Add Infinite Scrolling on Mobile Client

--- clients/mobile/lib/views/pages/posts_widget.dart
+++ clients/mobile/lib/views/pages/posts_widget.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:ramble_mobile/models/post_model.dart';
 import '../../controllers/user_controller.dart';
-import '../../themes/light_mode_theme.dart';
-import '../../utilities/utilities.dart';
 import '../reusable/post_widget.dart';
 import '../reusable/post_creator_widget.dart';
 
@@ -17,26 +15,24 @@ class PostsWidget extends StatefulWidget {
 
 class _PostsWidgetState extends State<PostsWidget> {
   late UserController _controller;
-  List<PostModel> posts = [];
-
-  //final PagingController<int, PostModel> _pagingController = PagingController(firstPageKey: 0);
+  final PagingController<int, PostModel> _page = PagingController(firstPageKey: 0);
 
   @override
   void initState() {
     _controller = widget.controller;
-    _loadPosts(0);
-    // _pagingController.addPageRequestListener((pageKey) {
-    //   _loadPosts(pageKey);
-    // });
+    _page.addPageRequestListener((pageKey) {
+      _loadPosts(pageKey);
+    });
     super.initState();
   }
 
   Future<void> _loadPosts(int pageKey) async {
     var newPosts = await _controller.getPosts(page: pageKey, category: "trending");
-    setState(() {
-      posts = newPosts;
-    });
-    // _pagingController.appendPage(newPosts, pageKey+1);
+    if (newPosts.isNotEmpty) {
+      _page.appendPage(newPosts, pageKey+1);
+    } else {
+      _page.appendLastPage([]);
+    }
   }
 
   @override
@@ -47,34 +43,30 @@ class _PostsWidgetState extends State<PostsWidget> {
           padding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 10.0),
           child: PostCreatorWidget(prompt: "What's new?", controller: _controller,
             onPost: () async {
-              await _loadPosts(0);
+              _page.refresh();
             }
           )
         ),
         Expanded(
-          child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: posts.length,
-            itemBuilder: (BuildContext context, int index) {
-                var post = posts[index];
-                return Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  child: PostWidget(
-                      key: Key(post.postId),
-                      post: post,
-                      controller: _controller
-                  ),
-                );
-            },
-          ),
+          child: PagedListView(
+              pagingController: _page,
+              builderDelegate: PagedChildBuilderDelegate<PostModel>(
+                  itemBuilder: (context, item, index) {
+                    return Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                        child: PostWidget(post: item, controller: _controller)
+                    );
+                  }
+              )
+          )
         ),
       ],
     );
   }
 
-  // @override
-  // void dispose() {
-  //   _pagingController.dispose();
-  //   super.dispose();
-  // }
+  @override
+  void dispose() {
+    _page.dispose();
+    super.dispose();
+  }
 }
