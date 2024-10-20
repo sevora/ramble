66d6db3ba8adeac661ad00da750ac5e09ee41fbc
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2024-10-20 18:20:45 +0800

    Update Mobile Client Environment

--- clients/mobile/lib/environment.dart
+++ clients/mobile/lib/environment.dart
@@ -1,6 +1,9 @@
 abstract class Environment {
   static const String serverURL = String.fromEnvironment(
       "SERVER_URL",
+
+      // This default value assumes that the server is on the local machine on port 8000.
+      // It also assumes that this client is on an emulator hence the 10.0.2.2 IP address.
       defaultValue: "10.0.2.2:8000"
   );
 }
